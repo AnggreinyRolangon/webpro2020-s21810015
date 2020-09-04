7e3eca10cf895b1e0a4ccf63c187178e1229055a
AnggreinyRolangon <anggreinyangelia30@gmail.com>
2020-09-04 15:04:16 -0700

    commit

---
+++ pertemuan03/index.js
@@ -0,0 +1,14 @@
+/*
+1. variabel dari tipe data
+*/
+
+const firstname =  "John";
+let age = 20;
+age = 21;
+const isMarried = true;
+
+console.log(firstname);
+console.log(age);
+console.log(isMarried);
+
+console.log("my name is " +  firstname +", i'm " + age  +" years old");
---
+++ pertemuan05/index.js
@@ -0,0 +1,13 @@
+let angka = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(angka);
+let buah = ["mangga", "apel", "jeruk"];
+console.loglog(buah);
+
+let arr = ["text", 1, true];
+console.log(arrr);
+
+let myfunc = function [] {
+    return "hello array";
+};
+
+let arr2 = []
