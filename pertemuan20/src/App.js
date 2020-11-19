6b9543af24a4df914d407e51b15599bde7ef10f7
AnggreinyRolangon <anggreinyangelia30@gmail.com>
2020-11-19 10:17:57 -0800

    commit

--- pertemuan20/src/App.js
+++ pertemuan20/src/App.js
@@ -1,55 +1,62 @@
-import React from 'react';
-import './App.css';
-const sortByPower = (a, b) => {
-    return b.power - a.power;
-}
-const List = props => {
-return (
-<>
-<h1>React People!</h1>
+// import React, {Component} from 'react';
+// import Card from "./Components/Cars";
+// import "./App.js"
 
-<ul className="list">
-{props.data.sort(sortByPower).map((person) => (
-<li key={person.name} className="list-item">
-<div className="list-item-image-container">
-<img src={person.thumbnail} />
-</div>
-<span className="list-item-name">
-{person.name} ( Power:{' '}
-<span className="list-item-power">{person.power}</span> )
-</span>
-</li>
-))}
-</ul>
-</>
-);
-}
+// class  App extends Component{
+//         state = {
+//                 users: [],
+
+//         };
+
+//         componentsDidMount() {
+//                 fetch ("https://jsonplaceholder.typicode.com/user")
+//                 .then((response) => response.json())     
+//                 .then((json) => this.setState({ users : json}));
+//         }
+
+//         render (){
+//                 return (
+//                 <>
+//                 {this.state.users.map((item) => {
+//                 return (
+//                 <Card
+//                 name={item.name}
+//                 usernmae={item.username}
+//                 email={item.email}
+//                 phone={item.phone}
+//                 />
+//                 );
+//                  })}
+//                 </>
+//           );
+//         }
+//     }
+// }
+// export default App;
+
 const App = () => {
-const human = [
-{
-name: 'Anj',
-thumbnail:
-        'https://www.facebook.com/photo?fbid=3641933419163435&set=pcb.3641933459163431',
-power: 10
-},
-{
-name: 'Elis',
-thumbnail: 
-        'https://www.facebook.com/photo?fbid=117656656738503&set=a.117656673405168',
-power: 3
-},
-{
-name: 'Valent',
-thumbnail:
-        'https://www.facebook.com/photo?fbid=1517340805008739&set=a.103905693018931',
+  const [user, setusers] = useState([]);
 
-power: 30
-}
-];
-return (
-<div className="App">
-<List data={human} />
-</div>
-);
-}
+  UseEffect(()=>{
+          axios
+          .get("https://jsonplaceholder.typicode.com/user")
+          .then((response) => setUser(response.data));
+        }, []);
+
+        return (
+         <>
+        {user.nap((item) => {
+        return (
+        <Card
+        name={item.name}
+        username={item.username}
+        email={item.email}
+        phone={item.phone}
+        />
+        );
+        })}
+        </>
+        );
+};
+
 export default App;
